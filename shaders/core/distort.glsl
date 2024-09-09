7cfbf0659a0f921c93edfeb343eb6dc4447c43c1
matcool <26722564+matcool@users.noreply.github.com>
2024-09-08 23:36:48 -0300

    add some silly options

--- shaders/core/distort.glsl
+++ shaders/core/distort.glsl
@@ -1,6 +1,11 @@
-#define SHADOW_DISTORT_FACTOR 0.08 // [0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1] Amount to distort shadow space by. Smaller values means higher quality near the player, but lower quality elsewhere.
+#define SHADOW_DISTORT_FACTOR 0.08 // [0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1]
+#define ENABLE_SHADOW_DISTORT
 
 vec2 distortShadowSpace(vec2 coordNDC) {
+#ifdef ENABLE_SHADOW_DISTORT
     float playerDist = length(coordNDC);
     return coordNDC / (SHADOW_DISTORT_FACTOR + playerDist);
+#else
+    return coordNDC;
+#endif
 }
